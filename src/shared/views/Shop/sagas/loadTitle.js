f170d77f822a4647b8c035707acc0c9c38545642
graymur <graymur@mail.ru>
2017-11-29 13:03:40 +0100

    Missing file

---
+++ src/shared/views/Shop/sagas/loadTitle.js
@@ -0,0 +1,12 @@
+import {call, put, takeLatest} from 'redux-saga/effects';
+import * as actions from '../actions.js';
+import dataSource from 'shared/dataSource';
+
+export function * loadTitle(action) {
+	const result = yield call(dataSource.fetchTitle);
+	yield put(actions.setTitle(result));
+}
+
+export default function * loadTitleSaga() {
+	yield takeLatest(actions.loadTitle.toString(), loadTitle);
+}
